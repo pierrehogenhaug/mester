e024bfe530b302b8489d157a8d956752b7b36ca8
pih <pih@capital-four.com>
2024-11-20 17:37:21 +0100

    update scripts to use capfourpy==1.0.1

--- scripts/data_collection/run_sharepoint_download.py
+++ scripts/data_collection/run_sharepoint_download.py
@@ -1,48 +1,60 @@
+import os
+import pandas as pd
+import sys
+
+# Add the project root directory to sys.path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.insert(0, project_root)
+
 from src.data_collection.sharepoint_scraper import CustomSharePoint, download_specific_files_for_row
 from src.data_processing.data_processing import process_sp_data, merge_data
-import pandas as pd
-import os
 
-# Read the processed data from the database extraction
-rms_with_fundamental_score = pd.read_csv('../data/processed/rms_with_fundamental_score.csv')
 
-# Function to get SharePoint data
-def get_sp_data():
-    ListId = "6ba7678f-2b65-4ad4-8759-21b68035c8c8"
-    SiteUrl = "https://c4.sharepoint.com/sites/IMP"
-    sp = CustomSharePoint(site_url=SiteUrl)
-    sp_data = sp.fetch_list_data(ListId=ListId)
-    return sp_data
+def main():
 
-# Fetch and process SharePoint data
-sp_data = get_sp_data()
-sp_data_unique = process_sp_data(sp_data)
+    # Read the processed data from the database extraction
+    rms_with_fundamental_score = pd.read_csv(os.path.join(project_root, 'data', 'rms_with_fundamental_score.csv'))
 
-# Merge the dataframes
-merged_data_unique = merge_data(sp_data_unique, rms_with_fundamental_score)
+    # Function to get SharePoint data
+    def get_sp_data():
+        ListId = "6ba7678f-2b65-4ad4-8759-21b68035c8c8"
+        SiteUrl = "https://c4.sharepoint.com/sites/IMP"
+        sp = CustomSharePoint(site_url=SiteUrl)
+        sp_data = sp.fetch_list_data(ListId=ListId, SiteUrl=SiteUrl)
+        return sp_data
 
-# Define the desired metadata values
-desired_document_type = "Legal"
-desired_document_subtype = "Offerings"
+    # Fetch and process SharePoint data
+    sp_data = get_sp_data()
+    sp_data_unique = process_sp_data(sp_data)
 
-# Create a dictionary to store CustomSharePoint instances
-sp_instances = {}
+    # Merge the dataframes
+    merged_data_unique = merge_data(sp_data_unique, rms_with_fundamental_score)
 
-# Process each row in the merged dataframe
-for index, row in merged_data_unique.iterrows():
-    if pd.isna(row['RmsId']):
-        continue  # Skip rows where there is no matching RmsId
-    # Download files; the function will create the folder only if files are downloaded
-    download_specific_files_for_row(row, desired_document_type, desired_document_subtype, sp_instances)
+    # Define the desired metadata values
+    desired_document_type = "Legal"
+    desired_document_subtype = "Offerings"
 
-# Print the list of RmsId that do not have Prospectus
-sorted_rms_id_df = merged_data_unique[["RmsId"]].sort_values(by="RmsId").reset_index(drop=True)
-rms_id_list = sorted_rms_id_df["RmsId"].to_list()
+    # Create a dictionary to store CustomSharePoint instances
+    sp_instances = {}
 
-directory_path = "../data/raw/sharepoint_reorg_files/"
-# Get list of RmsId folders that actually exist in the directory
-existing_folders = [int(folder) for folder in os.listdir(directory_path) if folder.isdigit() and int(folder) in rms_id_list]
+    # Process each row in the merged dataframe
+    for index, row in merged_data_unique.iterrows():
+        if pd.isna(row['RmsId']):
+            continue  # Skip rows where there is no matching RmsId
+        # Download files; the function will create the folder only if files are downloaded
+        download_specific_files_for_row(row, desired_document_type, desired_document_subtype)
 
-# List of RmsId that do not have an associated folder
-rms_id_without_folders = [rms_id for rms_id in rms_id_list if rms_id not in existing_folders]
-print("RmsIds without downloaded files:", rms_id_without_folders)
+    # Print the list of RmsId that do not have Prospectus
+    sorted_rms_id_df = merged_data_unique[["RmsId"]].sort_values(by="RmsId").reset_index(drop=True)
+    rms_id_list = sorted_rms_id_df["RmsId"].to_list()
+
+    directory_path = os.path.join(project_root, 'data', 'raw', 'sharepoint_reorg_files')
+    # Get list of RmsId folders that actually exist in the directory
+    existing_folders = [int(folder) for folder in os.listdir(directory_path) if folder.isdigit() and int(folder) in rms_id_list]
+
+    # List of RmsId that do not have an associated folder
+    rms_id_without_folders = [rms_id for rms_id in rms_id_list if rms_id not in existing_folders]
+    print("RmsIds without downloaded files:", rms_id_without_folders)
+
+if __name__ == "__main__":
+    main()
--- src/data_collection/sharepoint_scraper.py
+++ src/data_collection/sharepoint_scraper.py
@@ -1,18 +1,40 @@
-from capfourpy.authentication import get_access_token_interactive, get_azure_db_token_api
-from capfourpy.sharepoint import SharePoint
-from urllib.parse import urlparse
+# src/data_scraping/sharepoint_scraper.py
+
 import os
 import pandas as pd
 import platform
+import sys
+
+from urllib.parse import urlparse
+from capfourpy.authentication import get_access_token_interactive, get_azure_db_token_api
+from capfourpy.sharepoint import SharePoint
+
+# Add the project root directory to sys.path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.insert(0, project_root)
+
 
 class CustomSharePoint(SharePoint):
     """
     Subclass that inherits from capfourpy SharePoint
     """
+
     # Class variable to store the token
     idp_token = None
 
     def _generate_token(self, token: str = "missing idp token"):
+        """
+        Retrieves an authentication token using different methods based on the environment,
+        and caches it as a class-level variable to avoid repeated authentications.
+
+        Parameters
+        ----------
+        token (str, optional): Default token value, used when deployed. Defaults to "missing idp token".
+
+        Returns
+        -------
+        str: Authentication token for accessing SharePoint API.
+        """
         # Check if the token is already cached
         if CustomSharePoint.idp_token is not None:
             return CustomSharePoint.idp_token
@@ -31,18 +53,68 @@ class CustomSharePoint(SharePoint):
         CustomSharePoint.idp_token = token
         return token
 
+
     def fetch_list_data(self, ListId: str = None, SiteUrl: str = None) -> pd.DataFrame:
+        """
+        Retrieves all data from a specified SharePoint list and converts it to a DataFrame.
+
+        Parameters
+        ----------
+        ListId (str, optional): The unique identifier of the SharePoint list to retrieve.
+
+        Returns
+        -------
+        pd.DataFrame: DataFrame containing all items from the specified SharePoint list.
+        """
         large_list = self.ctx.web.lists.get_by_id(list_id=ListId)
+
+        # items = large_list.items.get().execute_query()
         items = large_list.items.get_all().execute_query()
+        # items = large_list.items.get_all().execute_query(500) # adding some number makes it run faster dunno why
         data = [item.properties for item in items]
+
         return pd.DataFrame(data)
 
+
     def get_files_metadata(self, folder_url: str):
+        """
+        Retrieves files in the specified SharePoint folder along with their metadata.
+
+        Parameters
+        ----------
+        folder_url : str
+            The relative URL of the target SharePoint folder.
+
+        Returns
+        -------
+        List
+            List of files with metadata in the specified folder.
+        """
         folder = self.ctx.web.get_folder_by_server_relative_url(folder_url)
+        # Expand to include ListItemAllFields to access metadata
         files_metadata = folder.files.expand(["ListItemAllFields"]).get().execute_query()
         return files_metadata
+    
 
-def download_specific_files_for_row(row, desired_document_type, desired_document_subtype, sp_instances):
+# Dictionary to store CustomSharePoint instances for each site URL to avoid redundant initializations
+sp_instances = {}
+
+def download_specific_files_for_row(row, desired_document_type, desired_document_subtype):
+    """
+    Downloads files with specific metadata values from the '/Reorg/' document library for a given site.
+    Saves the files into the specified output_folder.
+
+    Parameters
+    ----------
+    row : pd.Series
+        A row from the dataframe containing 'EB_SPWebUrl'.
+    desired_document_type : str
+        The desired value for the "Document Type" column.
+    desired_document_subtype : str
+        The desired value for the "Document SubType" column.
+    output_folder : str
+        The folder path where files will be saved.
+    """
     site_url = row['EB_SPWebUrl']
     rms_id = row['RmsId']
     if pd.isna(site_url):
@@ -74,7 +146,7 @@ def download_specific_files_for_row(row, desired_document_type, desired_document
                 file_name = file.name
 
                 # Define the output folder path
-                output_folder = os.path.join('../data/raw/sharepoint_reorg_files/', str(rms_id))
+                output_folder = os.path.join(project_root, 'data', 'raw', 'sharepoint_reorg_files', str(rms_id))
 
                 # Check if the file already exists
                 file_path = os.path.join(output_folder, file_name)
@@ -93,7 +165,8 @@ def download_specific_files_for_row(row, desired_document_type, desired_document
                     print(f"Downloaded file {file_name} from {site_url} to {output_folder}")
                     files_downloaded = True
                 else:
-                    pass  # File already exists
+                    pass
+                    #print(f"File {file_name} already exists in {output_folder}, skipping download.")
         if not files_downloaded:
             print(f"No new files to download for RmsId {rms_id} at {server_relative_url}")
     except Exception as e:
