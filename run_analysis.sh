7c77f5d3cc0ec43ed2baa86da2ef3556a4975f7e
s223730 <s223730@student.dtu.dk>
2024-12-15 10:53:03 +0100

    experimental additions (commented out for now)

--- run_analysis.sh
+++ run_analysis.sh
@@ -9,23 +9,23 @@
 ### -- Select the resources: 1 GPU in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set walltime limit: hh:mm -- maximum 24 hours for GPU queues
-#BSUB -W 0:15
+#BSUB -W 12:00
 ### -- request 4GB / 16GB of system-memory --
-#BSUB -R "rusage[mem=16GB]"
+#BSUB -R "rusage[mem=8GB]"
 ### -- set the email address --
 ##BSUB -u s223730@dtu.dk
-### -- send notification at start --
-#BSUB -B
-### -- send notification at completion --
-#BSUB -N
+### -- send notification at start --#BSUB -B
+### -- send notification at completion --#BSUB -N
 ### -- Specify the output and error file. %J is the job-id --
 #BSUB -o output_%J.out
 #BSUB -e error_%J.err
 # -- end of LSF options --
 
 # Load necessary modules
+# module unload gcc
+# module load gcc/10.3.0
 module load python3/3.10.14
-module load cuda/11.6
+module load cuda
 
 # Activate your virtual environment if needed
 source test_env/bin/activate
@@ -37,7 +37,7 @@ nvidia-smi --query-gpu=timestamp,name,utilization.gpu,utilization.memory,memory.
 NVSMI_PID=$!
 
 export MODEL_ID="mistralai/Mistral-7B-Instruct-v0.3"
-#export MODEL_ID="meta-llama/Llama-3.2-3B-Instruct"
+# export MODEL_ID="meta-llama/Llama-3.2-3B-Instruct"
 
 # Check if MODEL_ID is set
 if [ -z "$MODEL_ID" ]; then
@@ -49,6 +49,7 @@ fi
 echo "Using model: $MODEL_ID"
 
 # Run your Python script with the model_id argument
+# python scripts/analysis/run_analysis.py --model_id "$MODEL_ID"
 python scripts/analysis/run_analysis.py --model_id "$MODEL_ID"
 
 # After the script completes, kill the nvidia-smi logging process
